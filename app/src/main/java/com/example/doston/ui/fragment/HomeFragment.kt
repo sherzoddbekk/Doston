e42e8560231571852f95a322ee83465f87e807b2
sherzodbek <sherzodbek4758@gmail.com>
2023-05-23 15:54:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/doston/ui/fragment/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.doston.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.doston.R
+
+class HomeFragment:Fragment(R.layout.fragmnet_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+
+    private fun initView() {
+
+    }
+
+}
