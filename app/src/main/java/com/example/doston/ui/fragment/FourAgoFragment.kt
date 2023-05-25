7b46b2fe384066d819ed89e0cf2664dff6ccbc2b
sherzodbek <sherzodbek4758@gmail.com>
2023-05-25 11:25:13 +0500

    navigation

---
+++ app/src/main/java/com/example/doston/ui/fragment/FourAgoFragment.kt
@@ -0,0 +1,20 @@
+package com.example.doston.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.doston.R
+
+class FourAgoFragment:Fragment(R.layout.fragment_four_ago) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+
+    private fun initView() {
+
+    }
+
+}
