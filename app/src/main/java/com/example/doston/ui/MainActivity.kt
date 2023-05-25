c041ca75c4f4bd4916105abc395bda8b959e1d8a
sherzodbek <sherzodbek4758@gmail.com>
2023-05-25 12:28:02 +0500

    three

--- app/src/main/java/com/example/doston/ui/MainActivity.kt
+++ app/src/main/java/com/example/doston/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.doston.ui
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.doston.R
+import com.example.doston.databinding.ActivityMainBinding
+import com.example.doston.ui.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+   private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/example/doston/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/doston/ui/fragment/HomeFragment.kt
@@ -3,10 +3,14 @@ package com.example.doston.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.doston.R
+import com.example.doston.databinding.ActivityMainBinding
+import com.example.doston.databinding.FragmnetHomeBinding
 
-class HomeFragment:Fragment(R.layout.fragmnet_home) {
+class HomeFragment : Fragment(R.layout.fragmnet_home) {
 
+    private lateinit var binding: FragmnetHomeBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
@@ -15,6 +19,9 @@ class HomeFragment:Fragment(R.layout.fragmnet_home) {
 
     private fun initView() {
 
+        binding.tv3.setOnClickListener {
+            findNavController().navigate(R.id.threeAgoFragment)
+        }
     }
 
 }
